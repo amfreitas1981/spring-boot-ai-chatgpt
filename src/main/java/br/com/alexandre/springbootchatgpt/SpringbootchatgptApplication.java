fe083f9c8050262dbf8a2563ce2ec781ee679752
Alexandre Freitas <alexandremfreitas@gmail.com>
2024-08-02 13:01:13 -0300

    Update changes

---
+++ src/main/java/br/com/alexandre/springbootchatgpt/SpringbootchatgptApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexandre.springbootchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootchatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootchatgptApplication.class, args);
+	}
+
+}
